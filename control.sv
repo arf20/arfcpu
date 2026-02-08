f7bccad4d6fc8d6c81470bf450f8a1089b738319
arf20 <aruizfernandez05@gmail.com>
2026-02-08 13:31:34 +0100

    Enums

--- control.sv
+++ control.sv
@@ -24,58 +24,60 @@ module control (
 
     logic [3:0] state;
 
-    `define STATE_FETCH         4'd0    // 1
-    `define STATE_DECODE        4'd1    // 2
-    `define STATE_EXECUTE       4'd2    // 3a ALU operation
-    `define STATE_MEM_ADDR      4'd3    // 3b compute address imm
-    `define STATE_COND_BRANCH   4'd4    // 3c
-    `define STATE_JUMP          4'd5    // 3d
-    `define STATE_WRITEBACK_IMM 4'd6    // 3e
-    `define STATE_EXECUTE_IMM   4'd7    // 3f
-    `define STATE_WRITEBACK     4'd8    // 4a to register rd
-    `define STATE_MEM_READ      4'd9    // 4b
-    `define STATE_MEM_WRITE     4'd10   // 4c
-    `define STATE_WRITEBACK_RT  4'd11   // 4d
-    `define STATE_MEM_WRITEBACK 4'd12   // 5
+    typedef enum logic [3:0] {
+        STATE_FETCH,            // 1
+        STATE_DECODE,           // 2
+        STATE_EXECUTE,          // 3a ALU operation
+        STATE_MEM_ADDR,         // 3b compute address imm
+        STATE_COND_BRANCH,      // 3c
+        STATE_JUMP,             // 3d
+        STATE_WRITEBACK_IMM,    // 3e
+        STATE_EXECUTE_IMM,      // 3f
+        STATE_WRITEBACK,        // 4a to register rd
+        STATE_MEM_READ,         // 4b
+        STATE_MEM_WRITE,        // 4c
+        STATE_WRITEBACK_RT,     // 4d
+        STATE_MEM_WRITEBACK     // 5
+    } state_t;
 
     // transition function
     always_ff @(posedge clk or posedge reset)
         if (reset)
-            state = `STATE_FETCH;
+            state = STATE_FETCH;
         else case (state)
-            `STATE_FETCH: state = `STATE_DECODE;
+            STATE_FETCH: state = STATE_DECODE;
 
-            `STATE_DECODE:
+            STATE_DECODE:
                 case (inst_opcode)
-                    `OPCODE_ALUINST: state = `STATE_EXECUTE;
-                    `OPCODE_LOAD: state = `STATE_MEM_ADDR;
-                    `OPCODE_STORE: state = `STATE_MEM_ADDR;
-                    `OPCODE_BRANCH_EQ: state = `STATE_COND_BRANCH;
-                    `OPCODE_JUMP: state = `STATE_JUMP;
-                    `OPCODE_LOAD_IMM: state = `STATE_WRITEBACK_IMM;
-                    `OPCODE_OR_IMM: state = `STATE_EXECUTE_IMM;
-                    default: state = `STATE_FETCH;
+                    OPCODE_ALUINST: state = STATE_EXECUTE;
+                    OPCODE_LOAD: state = STATE_MEM_ADDR;
+                    OPCODE_STORE: state = STATE_MEM_ADDR;
+                    OPCODE_BRANCH_EQ: state = STATE_COND_BRANCH;
+                    OPCODE_JUMP: state = STATE_JUMP;
+                    OPCODE_LOAD_IMM: state = STATE_WRITEBACK_IMM;
+                    OPCODE_OR_IMM: state = STATE_EXECUTE_IMM;
+                    default: state = STATE_FETCH;
                 endcase
 
-            `STATE_EXECUTE: state = `STATE_WRITEBACK;
-            `STATE_MEM_ADDR:
+            STATE_EXECUTE: state = STATE_WRITEBACK;
+            STATE_MEM_ADDR:
                 case (inst_opcode)
-                    `OPCODE_LOAD: state = `STATE_MEM_READ;
-                    `OPCODE_STORE: state = `STATE_MEM_WRITE;
-                    default: state = `STATE_FETCH;
+                    OPCODE_LOAD: state = STATE_MEM_READ;
+                    OPCODE_STORE: state = STATE_MEM_WRITE;
+                    default: state = STATE_FETCH;
                 endcase
-            `STATE_COND_BRANCH: state = `STATE_FETCH;
-            `STATE_JUMP: state = `STATE_FETCH;
-            `STATE_WRITEBACK_IMM: state = `STATE_FETCH;
+            STATE_COND_BRANCH: state = STATE_FETCH;
+            STATE_JUMP: state = STATE_FETCH;
+            STATE_WRITEBACK_IMM: state = STATE_FETCH;
             
-            `STATE_WRITEBACK: state = `STATE_FETCH;
-            `STATE_MEM_READ: state = `STATE_MEM_WRITEBACK;
-            `STATE_MEM_WRITE: state = `STATE_FETCH;
-            `STATE_WRITEBACK_RT: state = `STATE_FETCH;
+            STATE_WRITEBACK: state = STATE_FETCH;
+            STATE_MEM_READ: state = STATE_MEM_WRITEBACK;
+            STATE_MEM_WRITE: state = STATE_FETCH;
+            STATE_WRITEBACK_RT: state = STATE_FETCH;
 
-            `STATE_MEM_WRITEBACK: state = `STATE_FETCH;
+            STATE_MEM_WRITEBACK: state = STATE_FETCH;
 
-            default: state = `STATE_FETCH;
+            default: state = STATE_FETCH;
         endcase
 
     // output function
@@ -97,7 +99,7 @@ module control (
         debug_state = state;
 
         case (state)
-            `STATE_FETCH: begin
+            STATE_FETCH: begin
                 iod = 1'd0;
                 memory_read = 1'd1;
                 write_ir = 1'd1;
@@ -107,61 +109,61 @@ module control (
                 pc_source = 2'd0;
                 write_pc = 1'd1;
             end
-            `STATE_DECODE: begin
+            STATE_DECODE: begin
                 alu_sel_a = 1'd0;
                 alu_sel_b = 3'd3;
                 alu_op = 2'd0;
             end
-            `STATE_EXECUTE: begin
+            STATE_EXECUTE: begin
                 alu_sel_a = 1'd1;
                 alu_sel_b = 3'd0;
                 alu_op = 2'd3;
             end
-            `STATE_MEM_ADDR: begin
+            STATE_MEM_ADDR: begin
                 alu_sel_a = 1'd1;
                 alu_sel_b = 3'd2;
                 alu_op = 2'd0;
             end
-            `STATE_COND_BRANCH: begin
+            STATE_COND_BRANCH: begin
                 alu_sel_a = 1'd1;
                 alu_sel_b = 3'd0;
                 alu_op = 2'd1;
                 pc_source = 2'd1;
                 write_pc_cond = 1'd1;
             end
-            `STATE_JUMP: begin
+            STATE_JUMP: begin
                 pc_source = 2'd2;
                 write_pc = 1'd1;
             end
-            `STATE_WRITEBACK_IMM: begin
+            STATE_WRITEBACK_IMM: begin
                 register_dest_sel = 1'd0;
                 data_source = 2'd2;
                 write_register = 1'd1;
             end
-            `STATE_EXECUTE_IMM: begin
+            STATE_EXECUTE_IMM: begin
                 alu_sel_a = 1'd1;
                 alu_sel_b = 3'd4;
                 alu_op = 2'd2;
             end
-            `STATE_WRITEBACK: begin
+            STATE_WRITEBACK: begin
                 register_dest_sel = 1'd1;
                 data_source = 2'd0;
                 write_register = 1'd1;
             end
-            `STATE_MEM_READ: begin
+            STATE_MEM_READ: begin
                 iod = 1'd1;
                 memory_read = 1'd1;
             end
-            `STATE_MEM_WRITE: begin
+            STATE_MEM_WRITE: begin
                 iod = 1'd1;
                 memory_write = 1'd1;
             end
-            `STATE_WRITEBACK_RT: begin
+            STATE_WRITEBACK_RT: begin
                 register_dest_sel = 1'd0;
                 data_source = 2'd0;
                 register_dest_sel = 1'd1;
             end
-            `STATE_MEM_WRITEBACK: begin
+            STATE_MEM_WRITEBACK: begin
                 register_dest_sel = 1'd0;
                 data_source = 2'd1;
                 write_register = 1'd1;
--- defines.sv
+++ defines.sv
@@ -2,13 +2,15 @@
 `define _DEFINES
 
 // opcodes
-`define OPCODE_ALUINST      6'd0    // add,sub,slt,and,or
-`define OPCODE_LOAD         6'd35   // lw
-`define OPCODE_STORE        6'd43   // sw
-`define OPCODE_BRANCH_EQ    6'd4    // beq
-`define OPCODE_JUMP         6'd2    // j
-`define OPCODE_LOAD_IMM     6'd15   // lui
-`define OPCODE_OR_IMM       6'd13   // ori
+typedef enum logic [5:0] {
+    OPCODE_ALUINST    = 6'd0,   // add,sub,slt,and,or
+    OPCODE_LOAD       = 6'd35,  // lw
+    OPCODE_STORE      = 6'd43,  // sw
+    OPCODE_BRANCH_EQ  = 6'd4,   // beq
+    OPCODE_JUMP       = 6'd2,   // j
+    OPCODE_LOAD_IMM   = 6'd15,  // lui
+    OPCODE_OR_IMM     = 6'd13   // ori
+} opcode_t;
 
 `endif
 
